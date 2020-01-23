b3d8e9843d3f6919fa7363e9f538f5da27cc2b75
walacetalesfs <walacetales@hotmail.com>
2020-01-23 15:20:44 -0300

    refactor: appModule

--- lib/app/shared/utils/app/app_module.dart
+++ lib/app/shared/utils/app/app_module.dart
@@ -1,24 +1,20 @@
 import 'package:dio/dio.dart';
-import 'package:doglife/app/shared/utils/app/app_controller.dart';
+import 'package:doglife/app/modules/auth/auth_module.dart';
 import 'package:doglife/app/shared/utils/app/app_widget.dart';
 import 'package:doglife/app/shared/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class AppModule extends MainModule{
+class AppModule extends MainModule {
   @override
   Widget get bootstrap => AppWidget();
 
   @override
-  List<Bind> get binds => [
-    Bind((i) => AppController(),),
-    Bind((i) => Dio(BaseOptions(baseUrl: BASE_URL)))
-  ];
+  List<Bind> get binds => [Bind((i) => Dio(BaseOptions(baseUrl: BASE_URL)))];
 
   @override
   List<Router> get routers => [
-    Router("/", child: (_, args) => Container(
-      child: Text('batata'),
-    ))
-  ];
+        Router('/', child: (_, args) => Container(child: Text('/'))),
+        Router('/auth', module: AuthModule())
+      ];
 }
