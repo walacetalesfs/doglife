d71babfeebe57f87dfebd6937e2197c6d7480c42
walacetalesfs <walacetales@hotmail.com>
2020-01-28 00:04:04 -0300

    refactor: posicionando componentes

--- lib/app/modules/auth/pages/cadastro/components/update_image.dart
+++ lib/app/modules/auth/pages/cadastro/components/update_image.dart
@@ -4,14 +4,14 @@ class UploadImageInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      alignment: Alignment(0, 1.3),
+      height: 150,
+      alignment: Alignment(0, 1.4),
       decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage('lib/assets/dog_gray.png'),
               fit: BoxFit.fitHeight)),
       child: Container(
-        height: 56,
+        height: 46,
         alignment: Alignment.center,
         child: Image.asset('lib/assets/icons/add.png'),
       ),
