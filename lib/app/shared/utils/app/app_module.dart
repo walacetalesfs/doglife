2730f576a3e32741ea1356b362bb7606ceb77f71
walacetalesfs <walacetales@hotmail.com>
2020-01-29 00:14:43 -0300

    feat: add splash_screen

--- lib/app/shared/utils/app/app_module.dart
+++ lib/app/shared/utils/app/app_module.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:doglife/app/modules/auth/auth_module.dart';
-import 'package:doglife/app/modules/home/splash_screen/splash_screen.dart';
+import 'package:doglife/app/modules/home/home_module.dart';
+import 'package:doglife/app/modules/home/pages/splash_screen/splash_screen.dart';
 import 'package:doglife/app/shared/utils/app/app_widget.dart';
 import 'package:doglife/app/shared/utils/constants.dart';
 import 'package:flutter/material.dart';
