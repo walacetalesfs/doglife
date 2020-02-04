ca9719efbb176c8dfd9199702fb8e89c922860de
walacetalesfs <walacetales@hotmail.com>
2020-02-04 00:57:11 -0300

    feat: add home screen with bottombar and appbar

--- lib/app/modules/home/home_widget.dart
+++ lib/app/modules/home/home_widget.dart
@@ -1,13 +1,53 @@
-import 'package:doglife/app/modules/home/components/appbar_home.dart';
+import 'package:doglife/app/modules/home/components/appbar/appbar.dart';
+import 'package:doglife/app/modules/home/components/bottombar/bottombar_items.dart';
 import 'package:flutter/material.dart';
 
-class HomeWidget extends StatelessWidget {
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(vsync: this, length: 5);
+    controller.addListener(_changeTabSelected);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void _changeTabSelected() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarHome(),
-      //body: PostsWidget(),
-      //bottomNavigationBar: BottomNavigationBar(),
-    );
+        appBar: getAppBar(controller.index),
+        body: TabBarView(
+          controller: controller,
+          children: <Widget>[
+            Container(color: Colors.white),
+            Container(color: Colors.black),
+            Container(color: Colors.blue),
+            Container(color: Colors.pink),
+            Container(color: Colors.grey)
+          ],
+        ),
+        bottomNavigationBar: TabBar(
+            indicatorColor: Colors.white,
+            indicatorWeight: 0.1,
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.grey,
+            controller: controller,
+            tabs: getTabsBottomBar(controller.index)));
   }
 }
